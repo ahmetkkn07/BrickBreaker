43bf2f7cc3632b747b8a832bc516de11c6729b04
Ahmet KÖKEN <ahmetkkn07@gmail.com>
2020-12-23 13:06:22 +0300

    First commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject pedal;
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Respawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Respawn()
+    {
+        //transform.position = new Vector3(0, -2f, 0);
+        //GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        transform.position = Vector3.zero;
+        GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle.normalized * speed;
+        pedal.transform.position = new Vector3(0, -4.5f, 0);
+    }
+}
---
+++ Assets/Scripts/WallDown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDown : MonoBehaviour
+{
+    public GameObject ball;
+    public Ball ballScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballScript = ball.GetComponent<Ball>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ballScript.Respawn();
+    }
+}
